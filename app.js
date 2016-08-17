29590d0c01eeedccb48fc9654cd97423fc88781e
ajzaff <ajzaff@gmail.com>
2016-08-16 21:04:33 -0400

    rename project, icon, change header

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get("/", function(req, res) {
 	res.render("pages/index", { title: "Index Page", message: "Hello, app!" });
 });
 app.listen(3000, function() {
-	console.log("groupol listening on port 3000!");
+	console.log("groupshare listening on port 3000!");
 });
