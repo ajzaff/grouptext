78e05722d7668f70052cc39af5a15763889f4db0
Alan Zaffetti <ajzaff@gmail.com>
2016-08-25 00:21:33 -0400

    listen on dynamic port

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", function(req, res) {
 	res.render("pages/index", { title: "Index Page" });
 });
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log("grouptext listening on port 3000!");
 });
