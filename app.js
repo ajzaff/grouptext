d483e36ed41202797310216ddc00b16675800b4d
Alan Zaffetti <ajzaff@gmail.com>
2016-08-27 19:18:16 -0400

    add basic login form

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set("views", path.join(__dirname, "views"));
 app.get("/", function(req, res) {
 	res.render("pages/index", { title: "Index Page" });
 });
+app.get("/signup", function(req, res) {
+	res.render("pages/signup");
+});
 app.listen(3000, function() {
 	console.log("grouptext listening on port 3000!");
 });
