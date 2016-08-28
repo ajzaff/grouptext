1a172a6b7adc8c6d5e717f6e847ff9dc69c96f37
Alan Zaffetti <ajzaff@gmail.com>
2016-08-27 22:09:37 -0400

    add ORM configuration to app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var path = require("path");
 
+/* Initialize ORM data models */
+require("./db/models.js");
+
 var app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
